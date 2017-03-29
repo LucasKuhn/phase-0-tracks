38159687c98a649267dd0337f8e101080cf61c9b
Lucas <lucasalexkuhn@gmail.com>
2017-03-29 15:49:57 -0300

    Completed 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -5,11 +5,28 @@ puts "What is his volume level?"
 volume = gets.chomp.to_i
 puts "What is its fur color?"
 color = gets.chomp
-puts "Is he a good candidate for adoption?"
-adoption = gets.chomp
 puts "What is his age?"
 age = gets.chomp.to_i
 if age == 0
   age = nil
+end
+puts "Is he a good candidate for adoption?"
+adoption = gets.chomp
+
+
+puts "Wellcome #{hamster}!"
+puts "I see your volume is #{volume}!"
+puts "I love to see your #{color} fur!"
+if age == nil
+  puts "You don't know your age?"
+else
+puts "And you are so young! Just #{age}!"
+end
+if adoption == 'yes'
+  puts "You are good for adoption"
+elsif adoption == 'no'
+  puts "Sadly we can't adopt you :("
+else 
+  puts "We are not sure what to do with you"
 end
 
