3e8493a6e65b629676b8b1883ee5ee0a3b616fd5
Lucas <lucasalexkuhn@gmail.com>
2017-03-30 20:24:04 -0300

    Did a simple loop for Solo Challenge, release 2

---
+++ ruby/vampires.rb
@@ -0,0 +1,43 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old will you be the end of this year?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "Would you like some fresh garlic bread?"
+bread = gets.chomp
+puts "Are in interested in the company's health insurance?"
+insurance = gets.chomp
+
+sentence = "Results inconclusive"
+loop do  
+  if age == 2017-year && (bread == 'yes' || insurance == 'yes')
+    sentence = "Probably not a vampire"
+  end
+  if age != 2017-year && (bread == 'no' || insurance == 'no')
+    sentence = "Probably a vampire"  
+  end
+  if age != 2017-year && bread == 'no' && insurance == 'no'
+    sentence = "Almost certainly a vampire"
+  end
+  if name == "Drake Cula" || name == "Tu Fang"
+    sentence = "Definitely a vampire"
+  end
+  break
+end
+puts "#{sentence}"
+
+# STETEMENTS BELOW JUST FOR TESTING 
+  # if age == 2017-year && (bread == 'yes' || insurance == 'yes')
+  #   puts "Probably not a vampire."
+  # end
+  # if age != 2017-year && (bread == 'no' || insurance == 'no')
+  #   puts "Probably a vampire."
+  # if age != 2017-year && bread == 'no' && insurance == 'no'
+  #   puts "Almost certainly a vampire."
+  # end
+  # if name == "Drake Cula" || name == "Tu Fang" 
+  # puts "Definitely a vampire."
+  # end
+  #   puts "Results inconclusive."
+  # end
