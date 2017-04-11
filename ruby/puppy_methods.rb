f10d826ba3169cff511fde2fc5c69f58e580abc9
Lucas <lucasalexkuhn@gmail.com>
2017-04-11 19:46:45 -0300

    Release 1 done

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,10 +4,33 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(times)
+    times.times do 
+      puts "Woof"
+    end
+  end
+  
+  def roll_over
+    puts "rolls over"
+  end
+  
+  def dog_year(human_years)
+    dog_years = human_years*7
+    puts "#{dog_years}"
+  end 
+  
+  def get_beer(brand)
+    puts "I got you some #{brand}"
+  end 
+  
+  def initialize
+    puts "Initilizing new puppy instance..."
+  end
 end
 
 # -- DRIVER CODE
-
 newpuppy = Puppy.new 
-
+newpuppy.fetch("ball")
+newpuppy.speak(3)
+newpuppy.dog_year(8)
+newpuppy.get_beer("Heineken")
