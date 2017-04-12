3ddde9371d5723ff4cbefca64be30b489009fff5
Lucas <lucasalexkuhn@gmail.com>
2017-04-11 23:30:57 -0300

    Finish up to Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,12 +2,11 @@
 class Santa
 
   def initialize (gender,ethnicity)
+    puts "Initializing Santa instance..."    
     @gender = gender
     @ethnicity = ethnicity
-    puts "Initializing Santa instance..."
-  end
-  age = 0
-  reindeer_ranking = [
+    @age = 0
+    @reindeer_ranking = [
     "Rudolph",
     "Dasher",
     "Dancer",
@@ -17,7 +16,10 @@ class Santa
     "Cupid",
     "Donner",
     "Blitzen"
-  ]
+    ]
+  end
+  
+ 
   def speak 
     puts "Ho, ho, ho! Haaaappy holidays!" 
   end
@@ -25,27 +27,73 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a great #{cookie_type}!"
   end
+  
+  def celebrate_birthday
+    @age += 1 
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking << reindeer_name
+  end
+
+  #SETTER METHODS (Writable)
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+  
+  #GETTER METHODS (Make things readable)
+  def age
+    @age
+  end
+  
+  def ethnicity
+    @ethnicity
+  end
+  
+  def gender
+    @gender
+  end
 
 end
 
-#--- TEST CODE
+#--- TEST CODE ----
 santa1 = Santa.new("male","British")
 santa1.speak
 santa1.eat_milk_and_cookies("Milk Cookie")
-p santa1.class
+santa1.get_mad_at("Vixen")
+p santa1.gender
+santa1.gender = "female"
+p santa1.gender
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+# ---- EXAMPLE: POSSIBLE SOLUTION 1 
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
-p santas
+# ---- EXAMPLE: POSSIBLE SOLUTION 2 
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
-
-
-
-#--- DRIVER CODE
+# ---- DRIVER CODE ----
+# santas =[]
+# puts "How many santas you want to add?"
+# amount = gets.chomp.to_i 
+# number = 1 
+# amount.times do 
+#   puts "Type the gender of santa #{number}:"
+#   gender = gets.chomp
+#   puts "Type the ethnicity of santa #{number}:"
+#   ethnicity = gets.chomp
+#   santas << Santa.new(gender,ethnicity)
+#   number += 1
+# end
