2e72097a09fb3a3f62d2fedd73105bf737543fa5
Lucas <lucasalexkuhn@gmail.com>
2017-04-28 13:31:31 -0300

    tested the kitten maker

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -2,12 +2,15 @@
 
 # talk about Object Relational Mapping (ORM)
 
-# require gems
+# require gems (Pre-Written Ruby)
 require 'sqlite3'
-require 'faker'
+require 'faker' #Gem that lets you create fake data
+# To install -> On terminal: <gem install faker>
 
-# create SQLite3 database
+# Create SQLite3 database
+#Bellow is the equivalent to sqlite3 kittens.db on terminal
 db = SQLite3::Database.new("kittens.db")
+# <.execute(something)> -> Takes the argument as sql and execute a command 
 db.results_as_hash = true
 
 # learn about fancy string delimiters
@@ -36,8 +39,11 @@ end
   create_kitten(db, Faker::Name.name, 0)
 end
 
-# explore ORM by retrieving data
+# --- AFTER CREATING 1 KITTEN ---
+# explore ORM (Object Relational Mapping) by retrieving data
 # kittens = db.execute("SELECT * FROM kittens")
+# puts kittens.class
+# p kittens
 # kittens.each do |kitten|
 #  puts "#{kitten['name']} is #{kitten['age']}"
 # end
