cd8e1720ca73ef5a05bff4899aa26241285b5308
Lucas <lucasalexkuhn@gmail.com>
2017-05-01 10:35:10 -0300

    Added initial code to create a table and add a command

--- databases/cheatsheet/maker.rb
+++ databases/cheatsheet/maker.rb
@@ -4,15 +4,28 @@
 #Requite Gems (SQLITE)
 
 #Create SQL Database
+db = SQLite3::Database.new("cheatsheet.db")
+db.results_as_hash = true
 
 #Use Fancy Strings Delimeters to create a command
-
-#Create the content table using the fancy command
+create_table = <<-SQL 
+	CREATE TABLE IF NOT EXISTS commands (
+	id INTEGER PRIMARY KEY,
+	title VARCHAR(255),
+	code VARCHAR(255)
+	)
+SQL
+#Create the content table using the super-fancy command
+db.execute(create_table)
 
 #Add persistent content to table 
+db.execute("INSERT INTO commands (title,code) VALUES ('To display tables','.tables')")
 
 #Show the content on the table
 # --- All The content
+commands = db.execute("SELECT * FROM commands")
+puts commands.class
+
 # --- Only selected content
 
 #Write the content to an HTML file
