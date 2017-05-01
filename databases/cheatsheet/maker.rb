57f99e63c955af89172567bc5a805da27fd673ce
Lucas <lucasalexkuhn@gmail.com>
2017-05-01 10:01:22 -0300

    Added Pseudo - Coding

---
+++ databases/cheatsheet/maker.rb
@@ -0,0 +1,23 @@
+#LET'S MAKE SOME CHEAT SHEETS!
+# --- The Project I envisioned is a program to make your own Cheat Sheets
+
+#Requite Gems (SQLITE)
+
+#Create SQL Database
+
+#Use Fancy Strings Delimeters to create a command
+
+#Create the content table using the fancy command
+
+#Add persistent content to table 
+
+#Show the content on the table
+# --- All The content
+# --- Only selected content
+
+#Write the content to an HTML file
+#--- Read more about IO Class
+
+#Create a GitHub Page for it
+#--- Look for: GH Project Page
+
